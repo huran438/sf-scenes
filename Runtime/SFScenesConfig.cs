79511effd389512313fe8ac93959c25d057787f7
i.khurani <huran438@gmail.com>
2024-08-03 00:06:19 +0300

    New SFConfigs logic

--- Runtime/SFScenesConfig.cs
+++ Runtime/SFScenesConfig.cs
@@ -6,39 +6,9 @@ using UnityEngine;
 
 namespace SFramework.Scenes.Runtime
 {
-    public sealed class SFScenesConfig : SFConfig, ISFConfigsGenerator
+    public sealed class SFScenesConfig : SFNodesConfig
     {
         public SFScenesGroupContainer[] Groups;
-
         public override ISFConfigNode[] Children => Groups;
-
-        public void GetGenerationData(out SFGenerationData[] generationData)
-        {
-            var groups = new HashSet<string>();
-            var scenes = new HashSet<string>();
-
-            foreach (var layer0 in Groups)
-            {
-                groups.Add($"{Id}/{layer0.Id}");
-                foreach (var layer1 in layer0.Scenes)
-                {
-                    scenes.Add($"{Id}/{layer0.Id}/{layer1.Id}");
-                }
-            }
-
-            generationData = new[]
-            {
-                new SFGenerationData
-                {
-                    FileName = "SFScenes",
-                    Properties = scenes
-                },
-                new SFGenerationData
-                {
-                    FileName = "SFScenesGroups",
-                    Properties = groups
-                }
-            };
-        }
     }
 }
